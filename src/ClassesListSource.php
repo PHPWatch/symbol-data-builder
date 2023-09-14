f6a6e6686c0e2d3952279c19586fa7733db79507
Ayesh Karunaratne <ayesh@aye.sh>
2023-09-15 01:30:01 +0800

    Class list: Ignore PHPWatch and Composer namespaces

--- src/ClassesListSource.php
+++ src/ClassesListSource.php
@@ -5,8 +5,24 @@ namespace PHPWatch\SymbolData;
 class ClassesListSource implements DataSourceInterface {
     const NAME = 'class';
     private function gatherData() {
-        return get_declared_classes();
+        $classes = get_declared_classes();
+        $return = [];
+
+        foreach ($classes as $class) {
+            if (strpos($class, 'Composer/') === 0) {
+                continue;
+            }
+
+            if (strpos($class, 'PHPWatch/') === 0) {
+                continue;
+            }
+
+            $return[] = $class;
+        }
+
+        return $return;
     }
+
     public static function getAllData() {
         return (new self())->gatherData();
     }
