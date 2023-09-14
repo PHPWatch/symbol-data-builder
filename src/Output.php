048b53c6be24054334846e388f4b127508163e8a
Ayesh Karunaratne <ayesh@aye.sh>
2023-09-15 00:37:11 +0800

    Remove sub dir patterns

--- src/Output.php
+++ src/Output.php
@@ -2,12 +2,10 @@
 
 namespace PHPWatch\SymbolData;
 class Output {
-    private $outputDir;
     private $data = [];
 
     private $dir;
-    public function __construct(string $outputDir, string $dir = 'scratch') {
-        $this->outputDir = $outputDir;
+    public function __construct(string $dir = 'scratch') {
         $this->dir = $dir;
     }
     public function addData(string $key, $data): void {
@@ -18,15 +16,10 @@ class Output {
         if (!is_dir($this->dir) && !mkdir($concurrentDirectory = $this->dir) && !is_dir($concurrentDirectory)) {
             throw new \RuntimeException(sprintf('Directory "%s" was not created', $concurrentDirectory));
         }
-        if (!is_dir($this->dir . '/' . $this->outputDir) && !mkdir(
-                $concurrentDirectory = $this->dir . '/' . $this->outputDir
-            ) && !is_dir($concurrentDirectory)) {
-                throw new \RuntimeException(sprintf('Directory "%s" was not created', $concurrentDirectory));
-            }
 
         foreach ($this->data as $key => $data) {
             $data = var_export($data, true);
-            file_put_contents($this->dir . '/' . $this->outputDir . '/' . $key . '.json', $data);
+            file_put_contents($this->dir . '/' . $key . '.json', $data);
         }
     }
 }
