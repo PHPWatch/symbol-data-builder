f718011f25027833ee7f6dd9e731d2dd2c398b57
Ayesh <Ayesh@users.noreply.github.com>
2023-09-15 16:54:33 +0000

    Update database

---
+++ data/8.2/attribute.php
@@ -0,0 +1,7 @@
+<?php 
+
+return array (
+  0 => 'ReturnTypeWillChange',
+  1 => 'AllowDynamicProperties',
+  2 => 'SensitiveParameter',
+)
---
+++ data/8.3/attribute.php
@@ -0,0 +1,8 @@
+<?php 
+
+return array (
+  0 => 'ReturnTypeWillChange',
+  1 => 'AllowDynamicProperties',
+  2 => 'SensitiveParameter',
+  3 => 'Override',
+)
