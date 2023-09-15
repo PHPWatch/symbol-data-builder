1edfef4862c9e36dea4325fa6d6c0408368a43bc
Ayesh Karunaratne <ayesh@aye.sh>
2023-09-15 15:16:52 +0800

    Update phpinfo-source to exclude envvars and build info

--- src/PHPInfoSource.php
+++ src/PHPInfoSource.php
@@ -6,7 +6,8 @@ class PHPInfoSource extends DataSourceBase {
     const NAME = 'phpinfo';
     protected function gatherData() {
         ob_start();
-        phpinfo();
+        // Do not include env of build info as they change in every build and run
+        phpinfo(INFO_CREDITS|INFO_LICENSE|INFO_MODULES|INFO_CONFIGURATION);
         return ob_get_clean();
     }
 }
