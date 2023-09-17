6038c26893e2759b05d32021757d452afa921b87
Ayesh Karunaratne <ayesh@aye.sh>
2023-09-17 14:48:50 +0800

    Remove data directory

--- data/8.2/attribute.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-return array (
-  0 => 'ReturnTypeWillChange',
-  1 => 'AllowDynamicProperties',
-  2 => 'SensitiveParameter',
-)
--- data/8.4/attribute.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-return array (
-  0 => 'ReturnTypeWillChange',
-  1 => 'AllowDynamicProperties',
-  2 => 'SensitiveParameter',
-  3 => 'Override',
-)
