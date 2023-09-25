80c441a7c9cff6ad3b12d9d38b483372428d52af
Ayesh Karunaratne <ayesh@aye.sh>
2023-09-26 05:13:08 +0800

    Update InterfacesListSource to skip PHPWatch and Composer namespaces
    Fixes #1

--- src/InterfacesListSource.php
+++ src/InterfacesListSource.php
@@ -5,6 +5,21 @@ namespace PHPWatch\SymbolData;
 class InterfacesListSource extends DataSourceBase {
     const NAME = 'interface';
     protected function gatherData() {
-        return get_declared_interfaces();
+        $interfaces = get_declared_interfaces();
+        $return = [];
+
+        foreach ($interfaces as $interface) {
+            if (strpos($interface, 'Composer\\') === 0) {
+                continue;
+            }
+
+            if (strpos($interface, 'PHPWatch\\') === 0) {
+                continue;
+            }
+
+            $return[] = $interface;
+        }
+
+        return $return;
     }
 }
