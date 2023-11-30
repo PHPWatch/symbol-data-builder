467a491a73d9a2b173ddfa5266f0865d441fbb86
Art4 <art4@wlabs.de>
2023-11-30 10:39:52 +0100

    Add details about interfaces, constants, properties and methods for interfaces

--- src/InterfacesListSource.php
+++ src/InterfacesListSource.php
@@ -35,14 +35,62 @@ class InterfacesListSource extends DataSourceBase {
                 ];
             }
 
+            $properties = [];
+
+            foreach ($reflection->getProperties() as $property) {
+                $properties[$property->getName()] = [
+                    'name' => $property->getName(),
+                    'class' => $property->getDeclaringClass()->getName(),
+                    'type' => ($property->getType() !== null) ? strval($property->getType()) : null,
+                    'has_default_value' => $property->hasDefaultValue(),
+                    'default_value' => $property->getDefaultValue(),
+                    'is_static' => $property->isStatic(),
+                    'is_public' => $property->isPublic(),
+                    'is_protected' => $property->isProtected(),
+                    'is_private' => $property->isPrivate(),
+                    'is_promoted' => $property->isPromoted(),
+                ];
+            }
+
+            $methods = [];
+
+            foreach ($reflection->getMethods() as $method) {
+                $parameters = [];
+
+                foreach($method->getParameters() as $parameter) {
+                    $parameters[$parameter->getName()] = [
+                        'position'=> $parameter->getPosition(),
+                        'name'=> $parameter->getName(),
+                        'type'=> ($parameter->getType() !== null) ? strval($parameter->getType()) : null,
+                        'is_optional' => $parameter->isOptional(),
+                        'has_default_value' => $parameter->isDefaultValueAvailable(),
+                        'default_value' => $parameter->isDefaultValueAvailable() ? $parameter->getDefaultValue() : null,
+                        'has_default_value_constant' => $parameter->isDefaultValueAvailable() && $parameter->isDefaultValueConstant(),
+                        'default_value_constant' => $parameter->isDefaultValueAvailable() && $parameter->isDefaultValueAvailable() ? $parameter->getDefaultValueConstantName() : null,
+                    ];
+                }
+
+                $methods[$method->getName()] = [
+                    'name' => $method->getName(),
+                    'class' => $method->getDeclaringClass()->getName(),
+                    'parameters' => $parameters,
+                    'return_type' => ($method->getReturnType() !== null) ? strval($method->getReturnType()) : null,
+                    'has_return_type' => $method->hasReturnType(),
+                    'is_static' => $method->isStatic(),
+                    'is_public' => $method->isPublic(),
+                    'is_protected' => $method->isProtected(),
+                    'is_private' => $method->isPrivate(),
+                ];
+            }
+
             $output->addData('interfaces/' . $filename, [
                 'type' => 'interface',
                 'name' => $reflection->getName(),
                 'meta' => $meta,
-                'interfaces' => [], // #todo
-                'constants' => [], // #todo
-                'properties' => [], // #todo
-                'methods' => [], // #todo
+                'interfaces' => $reflection->getInterfaceNames(),
+                'constants' => $reflection->getConstants(),
+                'properties' => $properties,
+                'methods' => $methods,
             ]);
         }
     }
