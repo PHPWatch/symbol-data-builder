450ba3e818689dd72a7755840ee736c14c2e83be
Art4 <art4@wlabs.de>
2023-12-06 11:42:17 +0100

    create tests for new dumper, add new Dumper and DataSource interfaces

---
+++ src/Dumper.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace PHPWatch\SymbolData;
+
+class Dumper {
+    /**
+     * @var DataSource[]
+     */
+    private $sources = [];
+
+    /**
+     * @var Output
+     */
+    private $output;
+
+    public function __construct(Output $output)
+    {
+        $this->output = $output;
+    }
+
+    public function addSource(DataSource $source): void
+    {
+        $this->sources[] = $source;
+    }
+
+    public function dump(): void {
+        foreach ($this->sources as $source) {
+            $source->addDataToOutput($this->output);
+        }
+
+        $this->output->write();
+    }
+}
