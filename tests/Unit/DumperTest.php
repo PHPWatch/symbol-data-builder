7d3bba0baa1be39df263961d2f8e30aaf465cef9
Art4 <art4@wlabs.de>
2023-12-06 11:42:17 +0100

    create tests for new dumper, add new Dumper and DataSource interfaces

---
+++ tests/Unit/DumperTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\PHPWatch\SymbolData;
+
+use PHPUnit\Framework\TestCase;
+use PHPWatch\SymbolData\DataSource;
+use PHPWatch\SymbolData\Dumper;
+use PHPWatch\SymbolData\Output;
+
+class DumperTest extends TestCase
+{
+    /**
+     * Tests that calling Dumper::dump() calls the sources and output.
+     *
+     * @covers Dumper
+     *
+     * @return void
+     */
+    public function testDumpCallsOutputWrite()
+    {
+        $output = $this->createMock(Output::class);
+        $output->expects($this->once())->method('write');
+
+        $source = $this->createMock(DataSource::class);
+        $source->expects($this->atLeastOnce())->method('addDataToOutput');
+
+        $dumper = new Dumper($output);
+        $dumper->addSource($source);
+
+        $dumper->dump();
+    }
+}
