03929ece8a6c3257e3373d814d7d1e00c53373a8
Ayesh Karunaratne <ayesh@aye.sh>
2024-08-02 04:06:06 +0700

    INI: add extension name to the `ini.php` file

--- src/Sources/INIListSource.php
+++ src/Sources/INIListSource.php
@@ -5,6 +5,7 @@ namespace PHPWatch\SymbolData\Sources;
 use PHPWatch\SymbolData\DataSource;
 use PHPWatch\SymbolData\DataSourceBase;
 use PHPWatch\SymbolData\Output;
+use ReflectionExtension;
 
 class INIListSource extends DataSourceBase implements DataSource {
     const NAME = 'ini';
@@ -23,6 +24,29 @@ class INIListSource extends DataSourceBase implements DataSource {
     }
 
     private static function handleIniList(array $iniList, Output $output) {
-        $output->addData('ini', $iniList);
+        $extList = get_loaded_extensions();
+
+        $indexedInis = array();
+
+        foreach ($extList as $name) {
+            $reflection = new ReflectionExtension($name);
+            $extIniEntries = $reflection->getINIEntries();
+            $indexedInis[$name] = $extIniEntries;
+        }
+
+        $iniListPool = $iniList;
+
+        foreach ($indexedInis as $ext => $extIniList) {
+            foreach ($extIniList as $iniName => $noOp) {
+                $indexedInis[$ext][$iniName] = $iniList[$iniName];
+                unset($iniListPool[$iniName]);
+            }
+        }
+
+        if (!empty($iniListPool)) {
+            throw new \RuntimeException('Uncategorized INI entries: '. print_r($iniListPool, true));
+        }
+
+        $output->addData('ini', $indexedInis);
     }
 }
